12dc88114e23871f635efaf4dfdca7a5b43a3eda
steevenakintilo <s.sangokuhomer7@gmail.com>
2026-05-14 21:51:56 +0200

    [ADD] Correct list of real people on wikipedia

--- src/wikisraping.py
+++ src/wikisraping.py
@@ -47,9 +47,9 @@ class WikiSrapping(Scrapdata):
     def __init__(self,headless=False):
         self.driver = Scrapdata(headless).driver
         self.all_file = self.print_file_content("../all_wikipedia_page_fr.txt").split("\n")
-        self.all_real_people = self.print_file_content(rf"{CODE_PATH}\real_people_dir\list_of_real_people_diff.txt").split("\n")
+        self.all_real_people = self.print_file_content("../all_wikipedia_page_fr.txt").split("\n")
         
-        self.size_of_batch_nb = 200
+        self.size_of_batch_nb = 10
         self.chunck_size = int(len(self.all_file)/self.size_of_batch_nb)
         self.zim = Archive("../wikipedia_fr_all_maxi_2026-02.zim")
 
@@ -149,31 +149,79 @@ class WikiSrapping(Scrapdata):
         return title.strip()
     
 
-    def is_reaal(self,page_name):
-        # Example: get article by title
+    def is_reaal(self, page_name):
         try:
             entry = self.zim.get_entry_by_title(self.clean_title(page_name))
 
-            # Read raw HTML
             html = entry.get_item().content.tobytes().decode("utf-8", errors="replace")
-            intro_text =  str(html[:10000])
-            if "Personnage de fiction apparaissant dans".lower() in intro_text.lower():
-                return False , f"https://fr.wikipedia.org/wiki/{page_name}"
-            
-            elif "biographie".lower() in intro_text.lower() and "naissance".lower() in intro_text.lower():
-                return True , f"https://fr.wikipedia.org/wiki/{page_name}"
-            else:
-                return False , f"https://fr.wikipedia.org/wiki/{page_name}"
+            text = html[:10000].lower()
+
+            url = f"https://fr.wikipedia.org/wiki/{page_name}"
+
+            # ---- FICTION ----
+            if (
+                "personnage de fiction apparaissant dans" in text or
+                "personnage de fiction" in text or
+                "personnage fictif" in text or
+                "personnage de jeu vidéo" in text or
+                "personnage de bande dessinée" in text or
+                "personnage de cinéma" in text or
+                "personnage de série télévisée" in text or
+                "catégorie:personnage de fiction" in text
+            ):
+                return False, url
+
+            # ---- VRAIE PERSONNE : infobox ----
+            if ((
+                    "nom de naissance" in text or
+                    "date de naissance" in text or
+                    "lieu de naissance" in text
+                ) and (
+                    "biographie" in text or
+                    "informations générales" in text
+                )
+            ) or (
+                
+                ("naissance" in text and "activité principale" in text) or 
+                ("biographie" in text and "naissance" in text) or 
+                ("biographie" in text and "décès" in text)
+            ):    return True, url
+
+            # ---- VRAIE PERSONNE : catégories ----
+            if (
+                "catégorie:naissance en " in text or
+                "catégorie:naissance à " in text or
+                "catégorie:naissance dans " in text or
+                "catégorie:décès en " in text or
+                "catégorie:décès à " in text or
+                "catégorie:personnalité" in text or
+                "catégorie:chanteur" in text or
+                "catégorie:chanteuse" in text or
+                "catégorie:acteur" in text or
+                "catégorie:actrice" in text or
+                "catégorie:écrivain" in text or
+                "catégorie:écrivaine" in text or
+                "catégorie:musicien" in text or
+                "catégorie:musicienne" in text or
+                "catégorie:homme politique" in text or
+                "catégorie:femme politique" in text or
+                "catégorie:scientifique" in text or
+                "catégorie:peintre" in text or
+                "catégorie:réalisateur" in text or
+                "catégorie:réalisatrice" in text
+            ):
+                return True, url
+
+            return False, url
+
         except:
-            #print("baaaaaad " , "titlee   " , page_name , " cleaaaned title " , self.clean_title(page_name))
-            #traceback.print_exc()
             return None, f"https://fr.wikipedia.org/wiki/{page_name}"
 
     def get_page_name_of_someone(self,page_name):
         # Example: get article by title
         try:
             entry = self.zim.get_entry_by_title(self.clean_title(page_name))
-
+            list_of_link = []
             # Read raw HTML
             html = entry.get_item().content.tobytes().decode("utf-8", errors="replace")
             soup = BeautifulSoup(html, "html.parser")
@@ -185,27 +233,111 @@ class WikiSrapping(Scrapdata):
                 if "title=" in link:
                     split_link_ = link.split("title=")[1].split(">")
                     link_name = split_link_[0].replace('"',"")
-                    if link_name in self.all_real_people:
-                        print(link_name)
-                        print("-"*50)
+                    if link_name in self.all_real_people and link_name not in list_of_link:
+                        list_of_link.append(link_name)
                     
             #print(soup.get_text()[:1000])
-
+            
+            print(list_of_link)
+            return link_name
             #print(intro_text.split("<a href="))
         except:
+            return ["error","Nan","404"]
             traceback.print_exc()
             return ""
-    def run_script(self):
+        
+    def merge_file(self,batch_nb):
+        dict_of_real_people = []
+        list_of_real_people = []
+        list_of_real_people_diff = []
+        
+        dict_of_non_real_people = []
+        list_of_non_real_people = []
+        
+        dict_of_error_people = []
+        list_of_error_people = []
+        
+        for i in range(1 , batch_nb + 1):
+            print(i)
+            dict_of_real_people+=self.print_file_content(rf"{CODE_PATH}\real_people_dir\dict_of_real_people{i}.txt").split("\n")
+            list_of_real_people+=self.print_file_content(rf"{CODE_PATH}\real_people_dir\list_of_real_people{i}.txt").split("\n")
+            list_of_real_people_diff+=self.print_file_content(rf"{CODE_PATH}\real_people_dir\list_of_real_people_diff{i}.txt").split("\n")
+
+            dict_of_non_real_people+=self.print_file_content(rf"{CODE_PATH}\non_real_people_dir\dict_of_non_real_people{i}.txt").split("\n")
+            list_of_non_real_people+=self.print_file_content(rf"{CODE_PATH}\non_real_people_dir\list_of_non_real_people{i}.txt").split("\n")
+            
+            dict_of_error_people+=self.print_file_content(rf"{CODE_PATH}\error_people_dir\dict_of_error_people{i}.txt").split("\n")
+            list_of_error_people+=self.print_file_content(rf"{CODE_PATH}\error_people_dir\list_of_error_people{i}.txt").split("\n")
+            
+        print("REAL PEOPLE")
+        for elem in dict_of_real_people:
+            if len(elem) == 0:
+                continue
+                
+            self.write_into_file(rf"{CODE_PATH}\real_people_dir\dict_of_real_people.txt",elem+"\n")
+        for elem in list_of_real_people:
+            if len(elem) == 0:
+                continue
+            
+            self.write_into_file(rf"{CODE_PATH}\real_people_dir\list_of_real_people.txt",elem+"\n")
+        for elem in list_of_real_people_diff:
+            if len(elem) == 0:
+                continue
+            
+            self.write_into_file(rf"{CODE_PATH}\real_people_dir\list_of_real_people_diff.txt",elem+"\n")
+        
+        print(len(dict_of_real_people))
+        print(len(list_of_real_people))
+        print(len(list_of_real_people_diff))
+        print("\n")
+
+        print("NON REAL PEOPLE")
+
+        for elem in dict_of_non_real_people:
+            if len(elem) == 0:
+                continue
+            
+            self.write_into_file(rf"{CODE_PATH}\non_real_people_dir\dict_of_non_real_people.txt",elem+"\n")
+        for elem in list_of_non_real_people:
+            if len(elem) == 0:
+                continue
+            
+            self.write_into_file(rf"{CODE_PATH}\non_real_people_dir\list_of_non_real_people.txt",elem+"\n")
+        
+        print(len(dict_of_non_real_people))
+        print(len(list_of_non_real_people))
+        print("\n")
+        
+        print("ERROR PEOPLE")
+
+        for elem in dict_of_error_people:
+            if len(elem) == 0:
+                continue
+            
+            self.write_into_file(rf"{CODE_PATH}\error_people_dir\dict_of_error_people.txt",elem+"\n")
+        for elem in list_of_error_people:
+            if len(elem) == 0:
+                continue
+            
+            self.write_into_file(rf"{CODE_PATH}\error_people_dir\list_of_error_people.txt",elem+"\n")
+        
+
+        print(len(dict_of_error_people))
+        print(len(list_of_error_people))
+        print("\n")
+
+        
+    def run_script(self,batch_nb):
         
         split_list = self.split_list(self.all_file,self.chunck_size)
-        #current_list = split_list[batch_nb]
+        current_list = split_list[batch_nb]
 
-        #already_done_page = self.print_file_content(rf"already_done_page_link{batch_nb}.txt").split("\n")
+        #already_done_page = self.print_file_content(rf"already_done_page_link{batch_nb + 1}.txt").split("\n")
 
-        for index , page in enumerate(self.all_file):
-            if index % 100000 == 0 and index > 0:
-                print(f"{page} {index}")
-
+        for index , page in enumerate(current_list):
+            if index % int(len(current_list) / 10) == 0:
+                print(f"{page} {index} {(index/int(len(current_list))) * 100}% done")
+            
             try:
                 is_real , page_link = self.is_reaal(page)
                 page_name = page
@@ -214,21 +346,45 @@ class WikiSrapping(Scrapdata):
                 info_dict = str({"page_name":page_name,"link":page_link,"is_real":is_real})
                 #print("goooood " , page) 
                 if is_real:
-                    self.write_into_file(rf"{CODE_PATH}\real_people_dir\dict_of_real_people.txt",info_dict+"\n")
-                    self.write_into_file(rf"{CODE_PATH}\real_people_dir\list_of_real_people.txt",page_name+"\n")
-                    self.write_into_file(rf"{CODE_PATH}\real_people_dir\list_of_real_people_diff.txt",self.clean_title(page_name)+"\n")
+                    self.write_into_file(rf"{CODE_PATH}\real_people_dir\dict_of_real_people{batch_nb + 1}.txt",info_dict+"\n")
+                    self.write_into_file(rf"{CODE_PATH}\real_people_dir\list_of_real_people{batch_nb + 1}.txt",page_name+"\n")
+                    self.write_into_file(rf"{CODE_PATH}\real_people_dir\list_of_real_people_diff{batch_nb + 1}.txt",self.clean_title(page_name)+"\n")
                 
                 elif is_real == False:
-                    self.write_into_file(rf"{CODE_PATH}\non_real_people_dir\dict_of_non_real_people.txt",info_dict+"\n")
-                    self.write_into_file(rf"{CODE_PATH}\non_real_people_dir\list_of_non_real_people.txt",page_name+"\n")
-                    
+                    self.write_into_file(rf"{CODE_PATH}\non_real_people_dir\dict_of_non_real_people{batch_nb + 1}.txt",info_dict+"\n")
+                    self.write_into_file(rf"{CODE_PATH}\non_real_people_dir\list_of_non_real_people{batch_nb + 1}.txt",page_name+"\n")
+
+                elif is_real == "Maybe":
+                    self.write_into_file(rf"{CODE_PATH}\maybe_real_people_dir\dict_of_maybe_real_people{batch_nb + 1}.txt",info_dict+"\n")
+                    self.write_into_file(rf"{CODE_PATH}\maybe_real_people_dir\list_of_maybe_real_people{batch_nb + 1}.txt",page_name+"\n")
+                  
                 else:
-                    self.write_into_file(rf"{CODE_PATH}\error_people_dir\dict_of_error_people.txt",info_dict+"\n")
-                    self.write_into_file(rf"{CODE_PATH}\error_people_dir\list_of_error_people.txt",page_name+"\n")
+                    self.write_into_file(rf"{CODE_PATH}\error_people_dir\dict_of_error_people{batch_nb + 1}.txt",info_dict+"\n")
+                    self.write_into_file(rf"{CODE_PATH}\error_people_dir\list_of_error_people{batch_nb + 1}.txt",page_name+"\n")
             except:
                 info_dict = str({})
-                self.write_into_file(rf"{CODE_PATH}\error_people_dir\dict_of_error_people.txt",info_dict+"\n")
-                self.write_into_file(rf"{CODE_PATH}\error_people_dir\list_of_error_people.txt",page_name+"\n")
-            
-x = WikiSrapping(True)
-x.get_page_name_of_someone("Albert_Ammons")
+                self.write_into_file(rf"{CODE_PATH}\error_people_dir\dict_of_error_people{batch_nb + 1}.txt",info_dict+"\n")
+                self.write_into_file(rf"{CODE_PATH}\error_people_dir\list_of_error_people{batch_nb + 1}.txt",page_name+"\n")
+
+
+
+a = 2
+
+if a == 0:
+    start = time.time()      
+    x = WikiSrapping(True)
+    x.get_page_name_of_someone("Albert_Ammons")
+    end = time.time()
+
+if a == 1:
+    x = WikiSrapping(True)
+    # x.merge_file(x.size_of_batch_nb)
+    x.run_script(int(sys.argv[1]))
+    end = time.time()
+
+    print(f"Total runtime of the program is {end - start} seconds")
+
+if a == 2:
+    x = WikiSrapping(True)
+    x.merge_file(x.size_of_batch_nb)
+
